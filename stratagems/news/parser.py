ca2ee8e45ddc8c34408499010570784b724d4eab
ilsurealism <earnmarks@yandex.ru>
2022-10-07 22:56:06 +0300

    Parser was updated.

--- stratagems/news/parser.py
+++ stratagems/news/parser.py
@@ -8,19 +8,41 @@ def parse_news():
     url = "https://vc.ru/"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    news_container = soup.find_all("div", class_="content-container")
+    news_container = soup.find_all("div", class_="feed__chunk")
+
+    for container in news_container:
+        container_detail = container.find_all("div", class_="content-container")
+        for item in container_detail:
+            news_detail_dict = {}
+            news_title = item.find("div", class_="content-title content-title--short l-island-a")
+            news_snippet = item.find("p")
+            news_title = news_title.string
+            if not news_title is None:
+                news_detail_dict["news_title"] = news_title.strip()
+            if not news_snippet is None:
+                news_snippet = news_snippet.get_text()
+                news_detail_dict["news_snippet"] = news_snippet
+            if len(news_detail_dict) != 0:
+                news_list.append(news_detail_dict)
+    return news_list
+
+
+def parse_tinkoff_journal():
+    news_list = []
+
+    url = "https://journal.tinkoff.ru/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    news_container = soup.find_all("div", class_="inner--BbvB6")
 
     for item in news_container:
         news_detail_dict = {}
-        news_title = item.find("div", class_="content-title content-title--short l-island-a")
-        news_snippet = item.find("div", class_="l-island-a")
-        news_title = news_title.string
+        news_title = item.find("h3", class_="title--hhlgn")
+
         if not news_title is None:
-            news_detail_dict["news_title"] = news_title.strip()
-        news_snippet = news_snippet.string
-        if not news_title is None:
-            news_detail_dict["news_snippet"] = news_snippet.strip()
-        news_list.append(news_detail_dict)
-    # result = soup.find_all("div", class_="content-title content-title--short l-island-a")
-    # print(result)
+            news_title = news_title.string
+            if len(news_title) > 0:
+                news_detail_dict["news_title"] = news_title.strip()
+        if len(news_detail_dict) != 0:
+            news_list.append(news_detail_dict)
     return news_list
